3c3467abdf7c04d6c0e3e5c1b77d756c3417b288
ManaHASEGAWA <hasegawa-mana607@g.ecc.u-tokyo.ac.jp>
2023-11-12 17:12:11 +0900

    6

--- hackathon/main.go
+++ hackathon/main.go
@@ -61,13 +61,8 @@ func init() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "application.json")
 
-	if r.Method == http.MethodOptions {
-		w.WriteHeader(http.StatusOK)
-		return
-	}
-	//この行を入れたらエラーが消えた
 	if r.Method == http.MethodOptions {
 		w.WriteHeader(http.StatusOK)
 		return
