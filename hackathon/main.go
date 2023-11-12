ae0888d9cb7e84c5fe985abe33339b17eb9ab1d0
ManaHASEGAWA <hasegawa-mana607@g.ecc.u-tokyo.ac.jp>
2023-11-12 16:59:39 +0900

    3

--- hackathon/main.go
+++ hackathon/main.go
@@ -59,17 +59,14 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Headers", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE,OPTION")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Origin", "https://hackathon-4-mana-hasegawa-front.vercel.app")
+	w.Header().Set("Access-Control-Allow-Origin", "https://hackathon-4-mana-hasegawa-front.vercel.app/")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-	//w.Header().Set("Access-Control-Allow-Origin", "*")
-	//w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	//w.Header().Set("Access-Control-Allow-Headers", "*")
-	//w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 	//この行を入れたらエラーが消えた
 	if r.Method == http.MethodOptions {
 		w.WriteHeader(http.StatusOK)
