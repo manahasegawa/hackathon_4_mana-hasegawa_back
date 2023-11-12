a150d5897689ed546cb2adfe0cd03402adece9e0
manahasegawa <141543876+manahasegawa@users.noreply.github.com>
2023-11-12 16:12:38 +0900

    Update main.go

--- hackathon/main.go
+++ hackathon/main.go
@@ -64,7 +64,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT,OPTION")
 	w.Header().Set("Access-Control-Allow-Origin", "https://hackathon-4-mana-hasegawa-front.vercel.app/")
-
+	w.Header().Set("Access-Control-Allow-Origin", "hackathon-4-mana-hasegawa-back-mbuoyt5akq-uc.a.run.app/")
+	
 	//w.Header().Set("Access-Control-Allow-Origin", "*")
 	//w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	//w.Header().Set("Access-Control-Allow-Headers", "*")
